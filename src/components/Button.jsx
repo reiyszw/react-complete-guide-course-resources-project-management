4ff698d78196e6905049f9c3e2e40ab0d50e7127
reiyszw <reiyszw@gmail.com>
2026-04-15 16:56:35 +0900

    Managing State to Switch Between Components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ children }) {
+export default function Button({ children, ...props }) {
   return (
-    <button className="px-4 py-2 text-xs md:text-base rounded bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+    <button
+      className="px-4 py-2 text-xs md:text-base rounded bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+      {...props}
+    >
       {children}
     </button>
   );
